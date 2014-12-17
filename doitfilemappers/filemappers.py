173a55ce2e5a987383430c15a49338518a002680
Gabriel Birke <gb@birke-software.de>
2014-12-17 13:19:32 +0100

    Allow for src to be optional
    This is in preparation for the CompositeMapper and ChainedMapper which
    need mapping information, but will get initialized classes without src
    attributes.

--- doitfilemappers/filemappers.py
+++ doitfilemappers/filemappers.py
@@ -5,30 +5,35 @@ import abc
 class BaseFileMapper(object):
     __metaclass__  = abc.ABCMeta
 
-    def __init__(self, src, callback=None, **kwargs):
+    def __init__(self, src="*", callback=None, **kwargs):
         self.map_initialized = False
         self.map = []
         self.src = src
         self.callback = callback
-        self.follow_symlinks = kwargs["follow_symlinks"] if "follow_symlinks" in kwargs else True
-        self.dir = kwargs["dir"] if "dir" in kwargs else "."
-        self.file_dep = kwargs["file_dep"] if "file_dep" in kwargs else True
+        self.follow_symlinks = kwargs.get("follow_symlinks", True)
+        self.dir = kwargs.get("dir", ".")
+        self.file_dep = kwargs.get("file_dep", True)
 
-    def get_map(self):
-        """ Return a list of (source, target) tuples. """
+    def get_map(self, src=None):
+        """
+        Return a list of (source, target) tuples.
+
+        Each source and target is an instance of Path.
+        """
         if not self.map_initialized:
-            self._initialize_map()
+            self.map = self._create_map(self.src)
             self.map_initialized = True
         return self.map
 
     @abc.abstractmethod
-    def _initialize_map(self):
-        """ Initialize self.map with a mapping that makes sense for this mapping class. """
+    def _create_map(self, src):
+        """ Create the mapping that specific for this mapping class. """
 
     def get_action(self):
-        """ Return a function that iterates over the map, calling the callback.
+        """
+        Return a function that iterates over the map, calling the callback.
 
-            The `targets` parameter of the generated function is ignored.
+        The `targets` parameter of the generated function is ignored.
         """
         file_map = self.get_map()
         callback = self.callback
@@ -49,13 +54,14 @@ class BaseFileMapper(object):
             task["file_dep"] = [str(s) for s in sources]
         return task
 
-    def _get_files_from_glob(self):
-        """ Get a list of files from the glob expression in self.src
+    def _get_files_from_glob(self, src):
+        """ 
+        Get a list of files from the glob expression in src.
 
-            If self.follow_symlinks is false, symlinks will be ignored, 
-            otherwise smylinks to files will be returned.
+        If self.follow_symlinks is false, symlinks will be ignored, 
+        otherwise smylinks to files will be returned.
         """
-        return [p for p in pathlib.Path(self.dir).glob(self.src) if self._is_file_or_symlink(p)]
+        return [p for p in pathlib.Path(self.dir).glob(src) if self._is_file_or_symlink(p)]
 
     def _is_file_or_symlink(self, f):
         if not self.follow_symlinks and f.is_symlink():
@@ -65,21 +71,21 @@ class BaseFileMapper(object):
 
 
 class IdentityMapper(BaseFileMapper):
-    def __init__(self, src, callback=None, **kwargs):
+    def __init__(self, src="*", callback=None, **kwargs):
         super(IdentityMapper, self).__init__(src, callback, **kwargs)
-        self.file_dep = kwargs["file_dep"] if "file_dep" in kwargs else False
+        self.file_dep = kwargs.get("file_dep", False)
 
-    def _initialize_map(self):
-        self.map = [(f, f) for f in self._get_files_from_glob()]
+    def _create_map(self, src):
+        return [(f, f) for f in self._get_files_from_glob(src)]
 
 class RegexMapper(BaseFileMapper):
-    def __init__(self, src, callback=None, search=r".*", replace=r"\0", **kwargs):
+    def __init__(self, src="*", callback=None, search=r".*", replace=r"\0", **kwargs):
         super(RegexMapper, self).__init__(src, callback, **kwargs)
         self.pattern = re.compile(search)
         self.replace = replace
 
-    def _initialize_map(self):
-        self.map = [(f, self._get_target_from_source(f)) for f in self._get_files_from_glob()]
+    def _create_map(self, src):
+        return [(f, self._get_target_from_source(f)) for f in self._get_files_from_glob(src)]
 
     def _get_target_from_source(self, source):
         return pathlib.Path(re.sub(self.pattern, self.replace, str(source)))
