e1132881dfb26af55b1022be1b52642c00fdf4df
Gabriel Birke <gb@birke-software.de>
2015-02-04 16:02:13 +0100

    Fix package URL in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='doit-filemappers',
       version='0.1',
       description='File mappers as task generators for the DoIt automation library',
-      url='http://github.com/storborg/funniest',
+      url='https://github.com/gbirke/doit-filemappers',
       author='Gabriel Birke',
       author_email='gabriel.birke@gmail.com',
       license='MIT',
